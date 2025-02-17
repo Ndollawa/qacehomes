de149cf1b9ee8e80e995280c667b9ce179a69b13
DamifeZion <damifeolaleye@gmail.com>
2025-02-17 17:48:18 +0100

    feat: removed the unused stuffs

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,11 +1,5 @@
 <script lang="ts">
-import { generateEndpoint } from '@/app/constants';
-import { handleThrowError } from '@/app/utils';
 
-handleThrowError("ERR_502");
-generateEndpoint('GET_PROPERTY_DETAILS', {
-   
-})
 </script>
 
 <template>
