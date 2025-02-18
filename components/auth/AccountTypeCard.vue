a3c65880ae11781a96bd8062f19dfff1c5dfd0d9
DamifeZion <damifeolaleye@gmail.com>
2025-02-18 09:45:39 +0100

    feat; Completed the auth account type card

--- components/auth/AccountTypeCard.vue
+++ components/auth/AccountTypeCard.vue
@@ -1,9 +1,17 @@
 <template>
 	<Card class="h-full">
 		<CardContent class="p-4 400:p-2 500:p-4 400:text-start">
-			<img :src="props.src" :alt="props.alt" class="size-full" />
+			<span class="rounded-[10px] bg-muted size-full overflow-hidden">
+				<img
+					:src="props.src"
+					:alt="props.alt"
+					class="size-full max-400:max-h-36"
+				/>
+			</span>
 
-			<CardTitle class="mt-3 !text-md font-medium leading-loose 400:mt-4 400:leading-normal">
+			<CardTitle
+				class="mt-3 !text-md font-medium leading-loose 400:mt-4 400:leading-normal"
+			>
 				{{ props.title }}
 			</CardTitle>
 
