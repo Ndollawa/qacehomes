bd5bd58107c3c8de4505dc915a48a4e54abe00a2
DamifeZion <damifeolaleye@gmail.com>
2025-02-18 11:38:49 +0100

    feat: minor changes added

--- composables/pages/use-register.ts
+++ composables/pages/use-register.ts
@@ -1,7 +1,24 @@
+import { AccountType } from "@/app/enums";
+import { registerSchema } from "@/app/props/schema";
+import { handleThrowError } from "@/app/utils";
+
 export const useRegister = () => {
 	useHead({
-      title: "Register"
-   })
-   
-   return {};
+		title: "Register",
+	});
+
+	const { accountType } = useRoute().params;
+
+	// If the account type mis-match the enums then error
+	onMounted(() => {
+		if (
+			accountType !== AccountType.Landlord &&
+			accountType !== AccountType.PropertyManager
+		) {
+			handleThrowError("ERR_404");
+		}
+	});
+	console.log(accountType);
+
+	return {};
 };
